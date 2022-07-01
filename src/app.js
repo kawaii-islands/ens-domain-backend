4cc693474ade19c925dbf8bfae45080da7bd3706
QuanPT <quan.pt@orai.io>
2022-07-01 22:06:13 +0700

    chore: deploy to kubernetes

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use(cors());
 app.options('*', cors());
 
 // v1 api routes
-app.use('/v1', routes);
+app.use('/ens-domain/v1', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
